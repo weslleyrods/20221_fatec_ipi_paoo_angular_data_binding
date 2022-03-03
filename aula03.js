9c968b897452708c5c52cd48d80bfd2e454a69a0
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-03-03 09:34:39 -0300

    closure, json e colecoes - adicionado comentarios

--- aula03.js
+++ aula03.js
@@ -1,47 +1,76 @@
-
 //AULA 03
-
-// function ola(){
-//     let nome = `João`
-//     return function(){
-//         console.log(`Ola, ${nome}`)
-//     }
-// }
-// let olaResult = ola()
-// //esse resultado aqui é callable
-// olaResult()
-
-// function saudacoesFactory(saudacao, nome){
-//     return function(){
-//         console.log(`${saudacao}, ${nome}`)
-//     }
-// }
-
-// let olaJoao = saudacoesFactory('Oi', 'João')
-// let tchauJoao  = saudacoesFactory('Tchau','João')
-// olaJoao()
-// tchauJoao()
 
 //Closure é o conjunto de uma função interna tem acesso as variaveis da função externa
 
-// function eAgora(){
-//     let cont = 1
-//     function f1(){
-//         console.log(cont)
-//     }
-//     cont++
-//     function f2(){
-//         console.log(cont)
-//     }
-//     return{f1, f2} //devolve objeto Json. Não daria pra devoler as duas funcoes, ma so objeto Json permite
+// *** CLOSURE com função sem parâmetro***
 
-// }
+/* function f() {
+    let nome = "João";
+    function g() {
+    console.log(nome);
+    }
+    g();
+}
+f(); */
 
-// let eAgoraResult = eAgora()
-// eAgoraResult.f1()
-// eAgoraResult.f2()
+// *** CLOSURE com função anônimas ***
 
-//AULA 03 - JSON: jAVASCRIPT OBJECT NOTATION
+/* function ola() {
+    let nome = `João`;
+    return function () {
+    console.log(`Ola, ${nome}`);
+    };
+}
+let olaResult = ola();
+//esse resultado aqui é callable
+olaResult(); */
+
+ // *** CLOSURE com parâmetro***
+
+//Exemplo 1
+/* const dobro = function (n) {
+     return n * 2;
+    }; 
+console.log(dobro(4));*/
+
+//Exemplo 2
+
+/* function saudacoesFactory(saudacao, nome){
+    return function(){
+        console.log(`${saudacao}, ${nome}`)
+    }
+}
+
+let olaJoao = saudacoesFactory('Oi', 'João')
+let tchauJoao  = saudacoesFactory('Tchau','João')
+olaJoao()
+tchauJoao() */
+
+// CLOSURE com contador++
+//Neste momento, a função eAgora() já executou por completo e a variável cont já foi incrementada.
+//Seu valor final é mantido e, assim, ambas funções f1() e f2() exibirão 2.
+
+/* function eAgora(){
+    let cont = 1
+    function f1(){
+        console.log(cont)
+    }
+    cont++
+    function f2(){
+        console.log(cont)
+    }
+    //devolve objeto Json. 
+    //Não daria pra devoler as duas funcoes de forma independente na chamada, mas o objeto Json permite
+    return{f1, f2} 
+    //return f1(), f2()
+}
+
+let eAgoraResult = eAgora()
+//chamando as funções de forma independente
+eAgoraResult.f1()
+eAgoraResult.f2() */
+
+//*** JSON: JAVASCRIPT OBJECT NOTATION ***
 
 /* let pessoa = {
     nome: 'João', //par chave/valor separados por virgula
@@ -49,10 +78,11 @@
 } 
 
 console.log("Me chamo " + pessoa.nome)
-console.log("Tenho " + pessoa["idade"]+" anos")
- */
-/* 
-const pessoaComEndereco = {
+console.log("Tenho " + pessoa["idade"]+" anos") */
+
+
+//Objeto JSON dentro de outro objeto JSON
+/*  const pessoaComEndereco = {
     nome: "Maria",
     idade: 21,
     endereco:{
@@ -72,21 +102,41 @@ console.log(`
     e moro na rua ${pessoaComEndereco.endereco['logradouro']},
     número ${pessoaComEndereco['endereco']['numero']}
 
+`)  */
+
+// *** Operador de desestruturização de objetos ***
+
+//exemplo 1 com desestruturização
+
+/* const {nome, idade, endereco} = pessoaComEndereco
+console.log(`
+    Meu nome é ${nome}, tenho ${idade} e moro na ${endereco.logradouro}, número ${endereco.numero}
 `) */
 
-//operador de desestruturização
-//exemplo
+//exemplo 2 com desestruturização
 
 /* const {endereco} = pessoaComEndereco
 const {logradouro} = endereco
-console.log(logradouro) */
-/* const {nome, idade} = pessoa
+console.log(logradouro)
+const {nome, idade} = pessoa
 console.log(nome)
 console.log(idade) */
 
+//exemplo SEM a desustruturização
 
-//cOLEÇÕES NO js COM jSON
+/* const nome = pessoaComEndereco.nome
+const idade= pessoaComEndereco.idade
+const logradouro = pessoaComEndereco.endereco.logradouro
+const numero = pessoaComEndereco.endereco.numero
+console.log(nome)
+console.log(idade)
+console.log(logradouro)
+console.log(numero) */
+
+
+//Coleções no JS com JSON
 //{} determina um objeto e [] vetor/ coleção json
+
 
 // const concessionaria = {
 //     cnpj: '000011111654-45',
@@ -110,61 +160,67 @@ console.log(idade) */
 //         {
 //             marca: "Volkswagen",
 //             modelo: "Nivus",
-//             anoDeFabricacao: 2020/* ,
-//             revisoes: [] é possivel colocar coleções dentro de coleções*/
+//             anoDeFabricacao: 2020,/*
+//             revisoes: [] //é possivel colocar coleções dentro de coleções*/
 //         }
 //     ]
-// }
-
-// //forEach do JS
-// for (let veiculo of concessionaria.veiculos){
-
-//     //operador de destruturização, usado para evitar repetição
-//     const {marca, modelo, anoDeFabricacao} = veiculo
-//     console.log("Marca: "+marca)
-//     console.log("Modelo: "+modelo)
-//     console.log("Ano: "+anoDeFabricacao)
-// }
-
-// //reescrever com for tradicional
-
-// for(let i = 0; i < concessionaria.veiculos.length; i++){
-//     //const {marca, modelo, anoDeFabricacao} = veiculo
-//     console.log(`
-//     Marca: ${concessionaria.veiculos[i].marca}
-//     Modelo ${concessionaria.veiculos[i]["modelo"]}
-//     Ano ${concessionaria.veiculos[i].anoDeFabricacao}
-//     `) 
-// }
-
-// //for tradicional com o operador de destruturização
-// for(let i = 0; i < concessionaria.veiculos.length; i++){
-//     const {marca, modelo, anoDeFabricacao} = concessionaria.veiculos[i]
-//     console.log("Marca: "+marca)
-//     console.log("Modelo: "+modelo)
-//     console.log("Ano: "+anoDeFabricacao)
-// }
+// } 
 
 
+// *** Iterando a coleção de JSONs com forEach e for tradicional ****
 
-let calculadora = {
+//forEach do JS
+/* for (let veiculo of concessionaria.veiculos){
+
+    //operador de destruturização, usado para evitar repetição
+    const {marca, modelo, anoDeFabricacao} = veiculo
+    console.log("Marca: "+marca)
+    console.log("Modelo: "+modelo)
+    console.log("Ano: "+anoDeFabricacao)
+} */
+
+//reescrita com for tradicional
+
+/* for(let i = 0; i < concessionaria.veiculos.length; i++){
+    //poderia ter utilizado a destruturização, consequentemente
+    //dipensando a repetição da chamada do indice [i]
+    //const {marca, modelo, anoDeFabricacao} = veiculo
+    console.log(`
+    Marca: ${concessionaria.veiculos[i].marca}
+    Modelo ${concessionaria.veiculos[i]["modelo"]}
+    Ano ${concessionaria.veiculos[i].anoDeFabricacao}
+    `)
+} */
+
+//for tradicional com o operador de destruturização
+
+/* for(let i = 0; i < concessionaria.veiculos.length; i++){
+    const {marca, modelo, anoDeFabricacao} = concessionaria.veiculos[i]
+    console.log("Marca: "+marca)
+    console.log("Modelo: "+modelo)
+    console.log("Ano: "+anoDeFabricacao)
+} */
+
+// *** Funções armazenadas Objetos JSON ***
+/* let calculadora = {
     soma: (a, b) => a + b,
     subtracao: function (a, b){
-        return a - b 
+        return a - b
     }
 }
 
 console.log(calculadora.soma(2, 2))
-console.log(calculadora.subtracao(2, 2))
+console.log(calculadora.subtracao(2, 2)) */
 
-//OUTRA FORMA 
-const soma = (a, b) => a + b
+//OUTRA FORMA
+/* const soma = (a, b) => a + b
 function subtracao (a, b){
     return a - b
 }
 let calculadora = {
-    soma: soma,
-    //soma, <- atalho para soma: soma,
+    //soma: soma, 
+    soma, //<- atalho para soma: soma,
     subtracao: subtracao
-
 }
+console.log(calculadora.soma(2,2))
+console.log(calculadora.subtracao(2, 2)) */
