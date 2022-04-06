95aa89756db9dadd6d2e2d18abde5e8d8cf00896
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-04-06 18:18:16 -0300

    introducao ao estudo de de data binding

---
+++ angular-data-binding/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  numero: number
+  
+  escolher(){
+    this.numero = Math.floor(Math.random()*6)+1
+  }
+
+/*   nome;
+  idade = 20
+  esconderCaixa = true
+
+  alterarNome(evento){
+    //console.log(nome)
+    console.log(evento.target.value)
+    this.nome = evento.target.value
+  }
+
+  adicionar(nomeInput){
+    console.log("Adicionando...")
+    console.log(nomeInput.value)
+    this.nome = nomeInput.value
+  } */
+
+  
+}
